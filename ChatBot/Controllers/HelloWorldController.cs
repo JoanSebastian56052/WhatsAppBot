34971897d75a73da25e00e511bc1245330f2868c
Joan Sebastian Morales Ocampo <Jmoraleso@intergrupo.com>
2019-09-11 12:29:39 -0500

    Despliegue

---
+++ ChatBot/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace ChatBot.Controllers
+{
+    public class HelloWorldController : TwilioController
+    {
+        [HttpPost]
+        public TwiMLResult Index(SmsRequest request)
+        {
+            var response = new MessagingResponse();
+            response.Message("Hello World");
+            return TwiML(response);
+        }
+    }
+}
